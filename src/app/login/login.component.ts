682a82d8a518e898d5d9605fdb640a5019116597
sergeome <sergeome@gmail.com>
2016-10-08 21:32:46 -0500

    Implemented login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from "../service/login.service";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,12 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  isLoginCorrect: any = true;
+  isAuthenticated: any = false;
   loginForm: FormGroup;
 
-  constructor(private loginService: LoginService) {
+  constructor(private loginService: LoginService, private router:Router) {
     this.loginForm = new FormGroup({
       'email': new FormControl('', [
         Validators.required,
@@ -19,12 +23,25 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  onSubmit(){
-    console.log( this.loginForm );
+  onLogin(){
+    this.isLoginCorrect = this.loginService.onLogin(this.loginForm.value)
+  }
+
+  onSignOut(){
+    this.loginService.onSignOut();
   }
 
   ngOnInit() {
     this.loginService.loginState(true);
+    this.loginService.isAuthorizedEmitter.subscribe(
+      (isAuthenticated) => {
+        this.isAuthenticated = isAuthenticated;
+      }
+    )
+  }
+
+  onRedirect(){
+    this.router.navigate(['/']);
   }
 
   ngOnDestroy(){
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,20 +1,56 @@
 import { Injectable, EventEmitter } from '@angular/core';
+import { User } from "../interfaces/user.interface";
+
+declare var firebase: any;
 
 @Injectable()
 
 export class LoginService {
-  signinUser(){
+  constructor() {}
 
-  }
-
-  constructor( ) {}
-
-  isLogin = false;
+  isLoginCorrect = true;
   loginEmitter = new EventEmitter<boolean>();
+  isAuthorizedEmitter = new EventEmitter<boolean>();
+  isUserAuthorized: any;
 
   loginState( loginState ) {
     this.loginEmitter.emit( loginState );
   }
+
+  onLogin( user: User ) {
+    var self = this;
+    firebase.auth().signInWithEmailAndPassword( user.email, user.password )
+      .then(function () {
+      console.log( "Service - User was signed in" );
+        self.isAuthorizedEmitter.emit(true);
+        return self.isLoginCorrect = false;
+    }, function (error) {
+        console.log( "Service - User wasn't signed in" );
+        console.log( error );
+        self.isAuthorizedEmitter.emit(false);
+      });
+  }
+
+  onSignOut( ) {
+    firebase.auth().signOut().then( function () {
+      console.log( "Service - User was sign out" );
+    }, function ( error ) {
+      console.log( error );
+      console.log( "Service - There was an error during singing out" );
+    } );
+  }
+
+  isAuthenticated() {
+    var self = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        self.isAuthorizedEmitter.emit(true);
+      } else {
+        self.isAuthorizedEmitter.emit(false);
+      }
+    });
+  }
+
 
 
 }
