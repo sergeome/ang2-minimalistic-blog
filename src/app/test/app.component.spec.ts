21b030e5263f1d331b86c19964b0604d59feeccf
sergeome <sergeome@gmail.com>
2016-10-10 22:29:05 -0500

    Moved app.component test file.

--- src/app/test/app.component.spec.ts
+++ src/app/test/app.component.spec.ts
@@ -1,23 +1,23 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { routing } from "./app.routing";
-import { HomePageComponent } from "./blog/homepage/homepage.component";
-import { HeaderComponent } from "./blog/header/header.component";
-import { FooterComponent } from "./blog/footer/footer.component";
-import { SearchComponent } from "./blog/header/search/search.component";
-import { MenuComponent } from "./blog/header/menu/menu.component";
-import { LoginComponent } from "./login/login.component";
-import { AddPostComponent } from "./blog/posts/add-post/add-post.component";
-import { AllPostsComponent } from "./blog/posts/all-posts/all-posts.component";
-import { SinglePostComponent } from "./blog/posts/single-post/single-post.component";
-import { NotFoundComponent } from "./blog/not-found/not-found.component";
+import { AppComponent } from '../app.component';
+import { routing } from "../app.routing";
+import { HomePageComponent } from "../blog/homepage/homepage.component";
+import { HeaderComponent } from "../blog/header/header.component";
+import { FooterComponent } from "../blog/footer/footer.component";
+import { SearchComponent } from "../blog/header/search/search.component";
+import { MenuComponent } from "../blog/header/menu/menu.component";
+import { LoginComponent } from "../login/login.component";
+import { AddPostComponent } from "../blog/posts/add-post/add-post.component";
+import { AllPostsComponent } from "../blog/posts/all-posts/all-posts.component";
+import { SinglePostComponent } from "../blog/posts/single-post/single-post.component";
+import { NotFoundComponent } from "../blog/not-found/not-found.component";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpModule } from "@angular/http";
-import { LoginService } from "./service/login.service";
-import { NotAuthorizedGuard } from "./blog/not-authorized.guard";
+import { LoginService } from "../service/login.service";
+import { NotAuthorizedGuard } from "../blog/not-authorized.guard";
 
 describe('App: Ang2Sergeblog', () => {
   beforeEach(() => {
