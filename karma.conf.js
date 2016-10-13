7697c4518375c4b24c55d48bb0c1cd7cb3bc3623
sergeome <sergeome@gmail.com>
2016-10-13 13:12:25 -0500

    Configured TestingModule for the app component in order to use "Fixture."

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: false
   });
 };
--- src/app/tests/app.component.spec.ts
+++ src/app/tests/app.component.spec.ts
@@ -1,11 +1,57 @@
 import { AppComponent } from "../app.component";
 import { LoginService } from "../service/login.service";
-
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { routing } from "../app.routing";
+import { HomePageComponent } from "../blog/homepage/homepage.component";
+import { HeaderComponent } from "../blog/header/header.component";
+import { FooterComponent } from "../blog/footer/footer.component";
+import { SearchComponent } from "../blog/header/search/search.component";
+import { MenuComponent } from "../blog/header/menu/menu.component";
+import { AllPostsComponent } from "../blog/posts/all-posts/all-posts.component";
+import { AddPostComponent } from "../blog/posts/add-post/add-post.component";
+import { SinglePostComponent } from "../blog/posts/single-post/single-post.component";
+import { LoginComponent } from "../login/login.component";
+import { NotFoundComponent } from "../blog/not-found/not-found.component";
 
 describe( 'AppComponent Test Suite', () => {
 
   let loginService = new LoginService();
   let appComponent = new AppComponent( loginService );
+
+  let comp:    BannerComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach( async(() => {
+      TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        HomePageComponent,
+        HeaderComponent,
+        MenuComponent,
+        SearchComponent,
+        FooterComponent,
+        AllPostsComponent,
+        AddPostComponent,
+        SinglePostComponent,
+        LoginComponent,
+        NotFoundComponent
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        routing
+      ],
+      providers: [
+        LoginService
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    comp    = fixture.componentInstance;
+  });
 
 
   it( "Login Service was initialized", () => {
