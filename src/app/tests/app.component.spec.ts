2908e5227ee19ecc959101497a2e8bca37b95fe6
sergeome <sergeome@gmail.com>
2016-10-13 15:20:01 -0500

    Implemented test for the app component.

--- src/app/tests/app.component.spec.ts
+++ src/app/tests/app.component.spec.ts
@@ -1,32 +1,10 @@
-import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { AppComponent } from "../app.component";
 import { LoginService } from "../service/login.service";
-import { ComponentFixture, TestBed, async } from "@angular/core/testing";
 
 describe( 'AppComponent Test Suite', () => {
 
   let loginService = new LoginService();
   let appComponent = new AppComponent( loginService );
-
-  let comp:    BannerComponent;
-  let fixture: ComponentFixture<AppComponent>;
-
-  beforeEach( async(() => {
-      TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-      providers: [
-        LoginService
-      ],
-      schemas: [ NO_ERRORS_SCHEMA ]
-    }).compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AppComponent);
-    comp    = fixture.componentInstance;
-  });
 
 
   it( "Login Service was initialized", () => {
@@ -36,6 +14,14 @@ describe( 'AppComponent Test Suite', () => {
   it( "App Component was initialized", () => {
     expect( appComponent ).toBeTruthy();
   } );
+
+  it('Observable test using EventEmitter ', done => {
+    loginService.loginEmitter.subscribe(x => {
+      expect(x).toBe(true);
+      done();
+    });
+    loginService.loginEmitter.emit(true);
+  });
 
   it( "Variable isLogin exist and it's false by default", () => {
     expect( appComponent.isLogin ).toBeFalsy();
@@ -49,12 +35,20 @@ describe( 'AppComponent Test Suite', () => {
     expect( appComponent.ngOnInit ).toBeTruthy();
   } );
 
+  it( "Function ngOnInit() returns undefined", () => {
+    expect( appComponent.ngOnInit() ).toBeUndefined();
+  } );
+
   it( "Function ngOnInit() contains loginEmitter from loginService", () => {
     expect( appComponent.ngOnInit ).toMatch("this.loginService.loginEmitter");
   } );
 
   it( "Function AfterViewInit() is exist", () => {
     expect( appComponent.ngAfterViewInit ).toBeTruthy();
+  } );
+
+  it( "Function AfterViewInit() returns undefined", () => {
+    expect( appComponent.ngAfterViewInit() ).toBeUndefined();
   } );
 
   it( "Function AfterViewInit() contains function isReady()", () => {
